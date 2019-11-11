d92a3db93ab5d2522601762a369176d46dd00a82
micro-chipset <tyrnov.ivan@gmail.com>
2019-11-11 16:34:57 +0300

    echo server header

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ var http = require('http');
 var url = require('url');
 
 var server = new http.Server(function (req, res) {
+
+    console.log(req.headers);
     console.log(req.method, req.url);
 
     var urlParsed = url.parse(req.url, true);
     console.log(urlParsed);
     if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.setHeader('Cache-control', 'no-cache');
         res.end(urlParsed.query.message)
     } else {
         res.statusCode = 404;
