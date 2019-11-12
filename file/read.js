5b56082fa660de80689fec6d7b3e79fbc4f9d083
micro-chipset <tyrnov.ivan@gmail.com>
2019-11-12 12:41:33 +0300

    module fs

---
+++ file/read.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.readFile(__filename, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data)
+    }
+});
